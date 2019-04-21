fd9a8ae20e31d7d1171b47ef506bbda4f6fbf87f
PCandeias <pedro.m94@hotmail.com>
2019-04-21 15:25:14 +0200

    Add maxpooling in atari preprocessing

--- wrappers/atari_preprocessing.py
+++ wrappers/atari_preprocessing.py
@@ -8,6 +8,7 @@ class AtariPreprocessing(gym.Wrapper):
         super(AtariPreprocessing, self).__init__(env)
         self.observation_space = spaces.Box(low=0, high=255, dtype=np.uint8, shape=(80,80))
         self.count = 0
+        self.last_obs = np.zeros((80,80))
 
     def __preprocessing(self, observation):
         observation = observation[35:195]
@@ -16,7 +17,10 @@ class AtariPreprocessing(gym.Wrapper):
         observation[observation == 109] = 0
         observation[observation != 0] = 1
 
-        return observation.astype(np.uint8)
+        n_observation = np.max(np.array([observation, self.last_obs]), axis=0)
+        self.last_obs = observation
+
+        return n_observation.astype(np.uint8)
 
     def step(self, action):
         self.count += 1
